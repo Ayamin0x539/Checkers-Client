de965f442800fef960cb1bc06825308c2dc2f37d
John Bojorquez <john.bojorquez@uconn.edu>
2016-03-28 17:05:59 -0400

    Added Player class.

---
+++ src/model/Player.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Player {
+	private ArrayList<Piece> pieces;
+	private String name;
+	
+	public Player(String name) {
+		this.name = name;
+		this.pieces = new ArrayList<Piece>();
+	}
+	
+	public void addPiece(Piece p) {
+		pieces.add(p);
+	}
+	
+	public ArrayList<Piece> getPieces() {
+		return pieces;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
