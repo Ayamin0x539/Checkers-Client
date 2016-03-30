4d27ee821f0ae554bf9fc97443fd19ac50683ead
John Bojorquez <john.bojorquez@uconn.edu>
2016-03-30 17:46:09 -0400

    Look and feel is now set to be cross platform

--- src/test/GUITest.java
+++ src/test/GUITest.java
@@ -1,10 +1,32 @@
 package test;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import view.CheckersWindow;
 
 public class GUITest {
 
 	public static void main(String[] args) {
+		
+		// Need to set the look and feel for this to be cross platform
+		try {
+			// Set cross-platform Java L&F (also called "Metal")
+			UIManager.setLookAndFeel(
+					UIManager.getCrossPlatformLookAndFeelClassName());
+		} 
+		catch (UnsupportedLookAndFeelException e) {
+			// handle exception
+		}
+		catch (ClassNotFoundException e) {
+			// handle exception
+		}
+		catch (InstantiationException e) {
+			// handle exception
+		}
+		catch (IllegalAccessException e) {
+			// handle exception
+		}
 		CheckersWindow window = new CheckersWindow();
 	}
 }
