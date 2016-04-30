a2f04195779277b300788b665a13b48352d84a93
aah13002 <aaron.huang@uconn.edu>
2016-04-30 00:55:35 -0400

    changed some constants

--- src/controller/GameConstants.java
+++ src/controller/GameConstants.java
@@ -4,7 +4,7 @@ import model.Color;
 
 public class GameConstants {
 	public static final int USER_MODE = 0;
-	public static final int SERVER_MODE = 0;
+	public static final int SERVER_MODE = 1;
 	public static final Color THUNK_COLOR = Color.WHITE;
 	public static final Color USER_COLOR = Color.BLACK;
 }
